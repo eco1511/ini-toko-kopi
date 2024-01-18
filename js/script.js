38251bdc7e3ccb8fbac2c024c1318b406c1b1eb6
eco1511 <eco1511@gmail.com>
2024-01-18 08:39:50 +0700

    part2 klik diluar element

--- js/script.js
+++ js/script.js
@@ -17,6 +17,20 @@ document.querySelector("#search").onclick = (e) => {
   e.preventDefault();
 };
 
+// klik diluar element
+const hm = document.querySelector("#hamburger-menu");
+const sb = document.querySelector("#search");
+
+document.addEventListener("click", function (e) {
+  if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+
+  if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
+  }
+});
+
 // klik diluar sidebar untuk menghilangkan nav
 const hamburger = document.querySelector("#hamburger-menu");
 

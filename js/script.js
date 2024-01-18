6e7311fab3d7907e06b93fd772fe191cf06c252e
eco1511 <eco1511@gmail.com>
2024-01-18 08:16:50 +0700

    part 2 menambahkan form search

--- js/script.js
+++ js/script.js
@@ -7,6 +7,16 @@ document.querySelector("#hamburger-menu").onclick = () => {
   navbarNav.classList.toggle("active");
 };
 
+// Toggle class active untuk search form
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+
+document.querySelector("#search").onclick = (e) => {
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  e.preventDefault();
+};
+
 // klik diluar sidebar untuk menghilangkan nav
 const hamburger = document.querySelector("#hamburger-menu");
 

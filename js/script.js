34c239531a4decd4f3bd35ad77fb8d63f7069c4b
eco1511 <eco1511@gmail.com>
2024-01-18 12:48:37 +0700

    part2 complate

--- js/script.js
+++ js/script.js
@@ -3,8 +3,9 @@
 const navbarNav = document.querySelector(".navbar-nav");
 //ketika hamburger menu di klik
 
-document.querySelector("#hamburger-menu").onclick = () => {
+document.querySelector("#hamburger-menu").onclick = (e) => {
   navbarNav.classList.toggle("active");
+  e.preventDefault();
 };
 
 // Toggle class active untuk search form
@@ -17,9 +18,18 @@ document.querySelector("#search").onclick = (e) => {
   e.preventDefault();
 };
 
+//toggle class active untuk shopping cart
+const shoppingCart = document.querySelector(".shopping-cart");
+
+document.querySelector("#shopping-cart").onclick = (e) => {
+  shoppingCart.classList.toggle("active");
+  e.preventDefault();
+};
+
 // klik diluar element
 const hm = document.querySelector("#hamburger-menu");
 const sb = document.querySelector("#search");
+const sc = document.querySelector("#shopping-cart");
 
 document.addEventListener("click", function (e) {
   if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
@@ -28,6 +38,10 @@ document.addEventListener("click", function (e) {
 
   if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
     searchForm.classList.remove("active");
+  }
+
+  if (!sc.contains(e.target) && !shoppingCart.contains(e.target)) {
+    shoppingCart.classList.remove("active");
   }
 });
 
@@ -39,3 +53,29 @@ document.addEventListener("click", function (e) {
     navbarNav.classList.remove("active");
   }
 });
+
+//MOdal box
+
+const itemDetailModal = document.querySelector("#item-detail-modal");
+const itemDetailButton = document.querySelectorAll(".item-detail-button");
+
+itemDetailButton.forEach((btn) => {
+  btn.onclick = (e) => {
+    itemDetailModal.style.display = "flex";
+    e.preventDefault();
+  };
+});
+
+// klik tombol close
+
+document.querySelector(".modal .close-icon").onclick = (e) => {
+  itemDetailModal.style.display = "none";
+  e.preventDefault();
+};
+
+//klik di luar modal
+window.onclick = (e) => {
+  if (e.target === itemDetailModal) {
+    itemDetailModal.style.display = "none";
+  }
+};
